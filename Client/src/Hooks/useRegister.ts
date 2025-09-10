4df2330f3fe9813cd04511e4d46c52da14a0a861
koliada24 <olehkoliada37@gmail.com>
2025-09-10 18:37:14 +0300

    Fixed redirection after registration

--- Client/src/Hooks/useRegister.ts
+++ Client/src/Hooks/useRegister.ts
@@ -2,6 +2,7 @@ import axios from './../axios';
 import type { RegisterUserDTO } from '../Models/RegisterUserDTO';
 import config from './../config';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../Contexts/AuthContext';
 
 interface useRegisterResult {
   handleRegistration: (registerUserDTO: RegisterUserDTO) => Promise<void>;
@@ -9,12 +10,14 @@ interface useRegisterResult {
 
 export function useRegister(): useRegisterResult {
   const navigate = useNavigate();
+  const { refreshAuth } = useAuth();
 
   const handleRegistration = async (registerUserDTO: RegisterUserDTO) => {
     try {
       const response = await axios.post(`${config.apiUrl}/identity/register`, registerUserDTO);
 
       if (response.status === 200) {
+        await refreshAuth();
         navigate('/home')
       }
       else {
