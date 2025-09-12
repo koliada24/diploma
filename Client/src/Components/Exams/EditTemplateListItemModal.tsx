f648954daa14e78f4b22a0fcc55e9821f8920d75
koliada24 <olehkoliada37@gmail.com>
2025-09-12 11:08:33 +0300

    Added "EditTemplateListItemModal" compoent

---
+++ Client/src/Components/Exams/EditTemplateListItemModal.tsx
@@ -0,0 +1,50 @@
+import { Button, Form, Modal } from "react-bootstrap";
+import type { ExamTemplate } from "../../Models/Exams/ExamTemplate";
+
+interface EditTemplateListItemModalProps {
+  template: ExamTemplate;
+  show: boolean;
+  handleHide: () => void;
+  handleSubmit: () => void;
+}
+
+export function EditTemplateListItemModal({ template, show, handleHide, handleSubmit }: EditTemplateListItemModalProps) {
+  return (
+    <Modal
+      show={show}
+      aria-labelledby="contained-modal-title-vcenter"
+      onHide={handleHide}
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Editing Exam Template
+        </Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+
+        <Form.Group className="mb-3" controlId="examtemplateform.title">
+          <Form.Label>Title</Form.Label>
+          <Form.Control placeholder="Title" value={template.title} />
+        </Form.Group>
+
+        <Form.Group controlId="examtemplateform.description">
+          <Form.Label>Exam Description</Form.Label>
+          <Form.Control placeholder="Description" value={template.description} />
+        </Form.Group>
+        
+      </Modal.Body>
+
+      <Modal.Footer className="justify-content-between">
+        <div className="d-flex justify-content-start">
+          <Button variant="danger" onClick={handleSubmit}>Delete</Button>
+        </div>
+        <div className="d-flex justify-content-end">
+          <Button variant="secondary" className="me-2" onClick={handleHide}>Cancel</Button>
+          <Button variant="primary" onClick={handleSubmit}>Submit</Button>
+        </div>
+      </Modal.Footer>
+    </Modal>
+  );
+} 
--- Client/src/Components/Exams/ExamTemplateListItem.tsx
+++ Client/src/Components/Exams/ExamTemplateListItem.tsx
@@ -1,16 +1,27 @@
 import { Col, ListGroup, Row } from "react-bootstrap";
 import type { ExamTemplate } from "../../Models/Exams/ExamTemplate";
+import { useState } from "react";
+import { EditTemplateListItemModal } from "./EditTemplateListItemModal";
 
 export function ExamTemplateListItem({ template }: { template: ExamTemplate }) {
   const { title, description, questionCount } = {...template};
+  const [ showEditExamTemplateModal, setShowEditExamTemplateModal ] = useState<boolean>(false);
 
   return(
-    <ListGroup.Item action>
-      <Row>
-        <Col xs={3}>{title}</Col>
-        <Col xs={8}>{description}</Col>
-        <Col xs={1} className="d-flex justify-content-center">{questionCount}</Col>
-      </Row>
-    </ListGroup.Item>
+    <>
+      <EditTemplateListItemModal
+        template={template}
+        show={showEditExamTemplateModal}
+        handleHide={() => setShowEditExamTemplateModal(false)}
+        handleSubmit={() => setShowEditExamTemplateModal(false)}
+      />
+      <ListGroup.Item action onClick={() => setShowEditExamTemplateModal(true)}>
+        <Row>
+          <Col xs={3}>{title}</Col>
+          <Col xs={8}>{description}</Col>
+          <Col xs={1} className="d-flex justify-content-center">{questionCount}</Col>
+        </Row>
+      </ListGroup.Item>
+    </>
   );
 }
