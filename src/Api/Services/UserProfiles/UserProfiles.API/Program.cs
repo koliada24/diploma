f9846e47077e60078e397448f01cdbbd1835c3b5
koliada24 <olehkoliada37@gmail.com>
2026-04-08 01:04:30 +0200

    Added UserProfiles service to docker-compose and Yarp

--- src/Api/Services/UserProfiles/UserProfiles.API/Program.cs
+++ src/Api/Services/UserProfiles/UserProfiles.API/Program.cs
@@ -1,5 +1,13 @@
+using Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.AddIdentity();
+
 var app = builder.Build();
+
+app.MapControllers();
+app.UseIdentity();
 
 app.Run();
